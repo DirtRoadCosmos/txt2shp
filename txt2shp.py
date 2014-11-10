ab26faddfa4daad149f619e130e6eae2fdc6f1af
andytilia <andytilia@users.noreply.github.com>
2014-11-10 12:44:35 -0500

    Allow optional bounding box to filter output

--- txt2shp.py
+++ txt2shp.py
@@ -2,56 +2,77 @@
 import csv
 import sys
 import os
-from shapely.geometry import Point, mapping
+from shapely.geometry import Point, Polygon, mapping
 from fiona import collection
 
-def createBbox(x1, y1, x2, y2):
-    print x1, y1, x2, y2
-    
-def createPoints(path, northwestx, northwesty, southeastx, southeasty):
-    bbox = createBbox(northwestx, northwesty, southeastx, southeasty)
-    
+class BBox(Polygon):
+    def __init__(self, minx, miny, maxx, maxy):
+        minx = float(minx)
+        miny = float(miny)
+        maxx = float(maxx)
+        maxy = float(maxy)
+        Polygon.__init__(self, [(minx, miny), (minx, maxy), (maxx, maxy), (maxx, miny)])
+
+
+def createPoints():
     schema = { 'geometry': 'Point', 'properties': {'postcode': 'str'} }
     with collection('points.shp', 'w', 'ESRI Shapefile', schema) as output:
-        with open(path, 'rb') as f:
+        with open(name, 'rb') as f:
             reader = csv.DictReader(f, delimiter = '|')
             for row in reader:
                 x = row['X-coord']
                 y = row['Y-coord']
                 if x and y:
                     point = Point(float(x), float(y))
-                    output.write({
-                        'properties': {
-                            'postcode': row['Main+Sub postal code']
-                        },
-                        'geometry': mapping(point)
-                    })
+                    if not 'thisBBox' in globals() or (thisBBox and point.within(thisBBox)):
+                        print 'added', point
+                        output.write({
+                            'properties': {
+                                'postcode': row['Main+Sub postal code']
+                            },
+                            'geometry': mapping(point)
+                        })                    
+                    else:
+                        print 'not added', point
+        # //find duplicate rule ids
+        # var unique_values = {};
+        # var duplicate_rule_ids = [];
+        # $.each(ruleList_raw, function(index, rule) { 
+                # ruleString = rule.id;
+                # if ( ! unique_values[ruleString] ) {
+                    # unique_values[ruleString] = true;
+                    # ruleList_unique.push(rule);
+                # } else {
+                    # duplicate_rule_ids.push(ruleString);
+                # }
+            # });
+        
+        # //examine the duplicates
+        # $.each(duplicate_rule_ids, function(index, value) {
+            # var result = $.grep(ruleList_raw, function(e){ return e.id === value; });
+            # console.log(result);
+        # });
+
 
 def usage():
-    s = 'txt2shp.py [txtfile | dir] northwestx northwesty southeastx southeasty'
+    s = 'txt2shp.py [txtfile | dir] minx miny maxx maxy'
     return s
     
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         name = sys.argv[1]
-        northwestx = 0
-        northwesty = 0
-        southeastx = 0
-        southeasty = 0
     elif len(sys.argv) == 6:
+        thisBBox = BBox(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
         name = sys.argv[1]
-        northwestx = sys.argv[2]
-        northwesty = sys.argv[3]
-        southeastx = sys.argv[4]
-        southeasty = sys.argv[5]
     else:
         print usage()
-    name = sys.argv[1]
+        sys.exit(0)
+    
     if os.path.isdir(name):
         print 'opening dir:', name
         for filename in glob.glob(name + '/*.txt'):
-            print filename
-            createPoints(name, northwestx, northwesty, southeastx, southeasty)
+            print '  file:', filename
+            createPoints()
     else:
         print 'opening file:', name
-        createPoints(name, northwestx, northwesty, southeastx, southeasty)
+        createPoints()
